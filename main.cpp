d04cc9189848b338878d2e532ea29fe376f10d81
Caden Wright <05cwright05@gmail.com>
2026-02-07 20:36:33 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+/*
+ * Author: Caden Wright
+ * this file will explore the recreation of the standard vector and a comparison in performance
+ * first i will restrict to only ints and see if specificity can beat the real deal
+*/
+#include <iostream>
+#include <vector>
+#include "int_vector.h"
